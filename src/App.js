69582fbdecf425c3ac0e0a37ac2a49efb29cd8d5
leochi777 <tzuchi0822@gmail.com>
2021-02-22 22:48:44 +0800

    test

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  function hello(){
+    alert("hello");
+  }
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <h1>test</h1>
+        <button onClick={hello}>click this</button>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
